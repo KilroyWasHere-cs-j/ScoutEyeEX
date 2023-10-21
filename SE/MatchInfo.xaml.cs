d3870c172830034aa1646e7827d6585f8689be0a
Gabriel Tower <gmtower1@gmail.com>
2023-10-21 17:15:20 -0400

    Need to fix logic around the Scout button, but I'm tired.

--- SE/MatchInfo.xaml.cs
+++ SE/MatchInfo.xaml.cs
@@ -2,8 +2,45 @@ namespace SE;
 
 public partial class MatchInfo : ContentPage
 {
-	public MatchInfo()
+	Match match;
+	public MatchInfo(string scoutName)
 	{
 		InitializeComponent();
+		match = new Match();
+		match.scoutName = scoutName;
 	}
+
+	private async void OnScoutButtonClicked(object sender, EventArgs e)
+	{
+        try
+        {
+            match.matchNumber = int.Parse(MatchNumberEntry.Text.ToString());
+            match.teamNumber = int.Parse(TeamNumberEntry.Text.ToString());
+        }
+        catch
+        {
+            await DisplayAlert("Alert", "Please insure that all the fields are filled out correctly.", "OK");
+            if(!AllianceLabel.Text.Contains("Pink"))
+            {
+                await Navigation.PushAsync(new ScoutingPage(match));
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Please select an alliance.", "OK");
+            }
+        }
+    }
+
+	private void OnAllianceToggledClicked(object sender, EventArgs e)
+	{
+        match.isBlue ^= true;
+        if (match.isBlue == true)
+        {
+            AllianceLabel.Text = "Blue Alliance";
+        }
+        else
+        {
+            AllianceLabel.Text = "Red Alliance";
+        }
+    }
 }
